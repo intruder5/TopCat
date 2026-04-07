d3f9e28c929a527f8e94843f0818a756d20437d5
intruder5 <rickgucci9@gmail.com>
2026-04-06 21:20:30 -0500

    idek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     const readyRef = useScrollAnimation()
 
     return (
-        <main>
+        <main className="max-w-7xl mx-auto w-full">
             <Nav />
             <section id="hero">
                 <Hero />
