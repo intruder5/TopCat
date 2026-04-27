b992759d7bfa8cb8d004e42e81be38859f417cdd
intruder5 <rickgucci9@gmail.com>
2026-04-26 23:03:15 -0500

    re-design

--- src/components/ui/imgblock.tsx
+++ src/components/ui/imgblock.tsx
@@ -4,7 +4,7 @@ export default function ImageBlock() {
     return (
         <div className="flex flex-row gap-3 w-full max-w-[320px] sm:max-w-[380px] mx-auto">
             <Half />
-            <img className="rounded-2xl pr-5 flex-1 h-auto object-cover self-stretch" src="fou.jpg" />
+            <img className="rounded-none pr-5 flex-1 h-auto object-cover self-stretch" src="fou.jpg" />
         </div>
     )
 }
